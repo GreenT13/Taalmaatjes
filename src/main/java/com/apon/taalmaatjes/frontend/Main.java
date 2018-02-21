88d76d6c346c02ab6978db22bfb291c288a27155
Rico Apon <rico.apon@hotmail.com>
2018-02-21 21:57:45 +0100

    [Overzicht vrijwilligers] Als ik op de knop Vrijwilligers druk, direct naar het eerste scherm (geen terug)

--- src/main/java/com/apon/taalmaatjes/frontend/Main.java
+++ src/main/java/com/apon/taalmaatjes/frontend/Main.java
@@ -5,7 +5,9 @@ import com.apon.taalmaatjes.frontend.transition.TransitionHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
 
+@SuppressWarnings("unused")
 public class Main {
 
     @FXML
@@ -13,6 +15,8 @@ public class Main {
 
     @FXML
     Tab tabVolunteer, tabStudent, tabTask, tabReport;
+
+    private boolean needToRefresh = false;
 
     @FXML
     public void initialize() {
@@ -29,11 +33,34 @@ public class Main {
                     } else if (newTab.equals(tabStudent)) {
                         TransitionHandler.getInstance().goToTab(TabEnum.STUDENTS);
                     } else if (newTab.equals(tabTask)) {
-                            TransitionHandler.getInstance().goToTab(TabEnum.TASKS);
+                        TransitionHandler.getInstance().goToTab(TabEnum.TASKS);
                     } else if (newTab.equals(tabReport)) {
                         TransitionHandler.getInstance().goToTab(TabEnum.REPORTS);
                     }
+                    needToRefresh = false;
                 }
         );
     }
+
+    @FXML
+    public void onMouseClick(MouseEvent mouseEvent) {
+        if (!needToRefresh) {
+            needToRefresh = true;
+            return;
+        }
+
+        // Refresh
+        switch(tabPaneMain.getSelectionModel().getSelectedIndex() ){
+            case 0:
+                TransitionHandler.getInstance().goToTab(TabEnum.VOLUNTEERS);
+            case 1:
+                TransitionHandler.getInstance().goToTab(TabEnum.STUDENTS);
+            case 2:
+                TransitionHandler.getInstance().goToTab(TabEnum.TASKS);
+            case 3:
+                TransitionHandler.getInstance().goToTab(TabEnum.REPORTS);
+        }
+        System.out.println("Refreshed " + tabPaneMain.getSelectionModel().getSelectedIndex());
+    }
+
 }
