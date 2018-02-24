b78e088fe860dd33fc3999b030996dc08d962b4b
Rico Apon <rico.apon@hotmail.com>
2018-02-24 20:37:47 +0100

    [Overzicht vrijwilligers] Als ik op de knop Vrijwilligers druk, direct naar het eerste scherm (geen terug)

--- src/main/java/com/apon/taalmaatjes/frontend/Main.java
+++ src/main/java/com/apon/taalmaatjes/frontend/Main.java
@@ -5,6 +5,7 @@ import com.apon.taalmaatjes.frontend.transition.TransitionHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
 
 @SuppressWarnings("unused")
 public class Main {
@@ -14,6 +15,9 @@ public class Main {
 
     @FXML
     Tab tabVolunteer, tabStudent, tabTask, tabReport;
+
+    private boolean needToRefresh = true;
+    private TabEnum currentTabEnum = TabEnum.VOLUNTEERS;
 
     @FXML
     public void initialize() {
@@ -27,14 +31,34 @@ public class Main {
                 (ov, oldTab, newTab) -> {
                     if (newTab.equals(tabVolunteer)) {
                         TransitionHandler.getInstance().goToTab(TabEnum.VOLUNTEERS);
+                        currentTabEnum = TabEnum.VOLUNTEERS;
                     } else if (newTab.equals(tabStudent)) {
                         TransitionHandler.getInstance().goToTab(TabEnum.STUDENTS);
+                        currentTabEnum = TabEnum.STUDENTS;
                     } else if (newTab.equals(tabTask)) {
                         TransitionHandler.getInstance().goToTab(TabEnum.TASKS);
+                        currentTabEnum = TabEnum.TASKS;
                     } else if (newTab.equals(tabReport)) {
                         TransitionHandler.getInstance().goToTab(TabEnum.REPORTS);
+                        currentTabEnum = TabEnum.REPORTS;
                     }
+                    needToRefresh = false;
                 }
         );
+    }
+
+    @FXML
+    public void onMouseClick(MouseEvent mouseEvent) {
+        if (!needToRefresh) {
+            needToRefresh = true;
+            return;
+        }
+
+        if (mouseEvent.getSceneX() > 150) {
+            return;
+        }
+
+        // Refresh current tab.
+        TransitionHandler.getInstance().goToTab(currentTabEnum);
     }
 }
